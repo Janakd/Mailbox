45ff0e0d0f75af119f625ea3e647e837f1e4a075
Janakd <janakd@gmail.com>
2015-11-14 20:20:58 -0800

    interactions without animations

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -18,15 +18,26 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var archiveIcon: UIImageView!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var messageView: UIImageView!
+    @IBOutlet weak var rightIconView: UIView!
+    @IBOutlet weak var leftIconsView: UIView!
+    
+    @IBOutlet weak var feedImageView: UIImageView!
+    @IBOutlet weak var listView: UIImageView!
+    
+    @IBOutlet weak var rescheduleView: UIImageView!
     
     var initialCenter: CGPoint!
+    var initialCenterRightIcons: CGPoint!
+    var initialCenterLeftIcons: CGPoint!
+    var endState = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scrollView.contentSize = CGSize(width: 320, height: 1202)
-
+        listView.alpha = 0
+        rescheduleView.alpha = 0
         
     }
 
@@ -47,10 +58,10 @@ class MailboxViewController: UIViewController {
         if sender.state == UIGestureRecognizerState.Began {
             
             self.initialCenter = messageView.center
-            archiveIcon.alpha = 0
-            laterIcon.alpha = 0
-            deleteIcon.alpha = 0
-            listIcon.alpha = 0
+            self.initialCenterLeftIcons = leftIconsView.center
+            self.initialCenterRightIcons = rightIconView.center
+            
+            resetAll()
            
             
         }
@@ -58,82 +69,130 @@ class MailboxViewController: UIViewController {
         else if sender.state == UIGestureRecognizerState.Changed {
             
             messageView.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y)
+        
             
+                //GREY RIGHT
             
-            //GO RIGHT
-            
-            if velocity.x >= 0 {
+            if translation.x > -60 && translation.x < 0 {
                 
-                //Animate
-                
-                archiveIcon.alpha = 1
-            }
-            
-            //GO LEFT
-                
-            else if velocity.x < 0 {
-                
-                //Animate
-                
+                backgroundView.backgroundColor = UIColor.grayColor()
                 laterIcon.alpha = 1
-            }
-            
-            
-            
-                //MAKE GREY AND SET ICONS TO TRANSPARENT
-                
-                if translation.x > -60 && translation.x < 60 {
-                    self.backgroundView.backgroundColor = UIColor.grayColor()
-                   
-                    
-                }
-                
-                //MAKE GREEN ARCHIVE ICON TRANSLATE
-                
-                else if translation.x >= 60 {
-                    backgroundView.backgroundColor = UIColor.greenColor()
-                    
+                endState = 0
+
                 }
             
-                //MAKE RED + DELETE ICON TRANSLATE
+                //GREY LEFT
+                
+            if  translation.x > 0 &&  translation.x < 60 {
+                
+                backgroundView.backgroundColor = UIColor.grayColor()
+                archiveIcon.alpha = 1
+                endState = 0
+
+                }
             
-                else if translation.x >= 260 {
-                    backgroundView.backgroundColor = UIColor.redColor()
+                //GREEN ARCHIVE ICON TRANSLATE
+                
+            else if translation.x >= 60 && translation.x < 260 {
+                backgroundView.backgroundColor = UIColor.greenColor()
+                resetAll()
+                archiveIcon.alpha = 1
+                leftIconsView.center = CGPoint(x: initialCenterLeftIcons.x + translation.x - 60, y: initialCenterLeftIcons.y)
+                endState = 0
+                
+            }
+            
+                //RED + DELETE ICON TRANSLATE
+            
+            else if translation.x >= 260 {
+                backgroundView.backgroundColor = UIColor.redColor()
+                resetAll()
+                deleteIcon.alpha = 1
+                leftIconsView.center = CGPoint(x: initialCenterLeftIcons.x + translation.x - 60, y: initialCenterLeftIcons.y)
+                endState = 0
             }
                 
+                //YELLOW + LATER ICON TRANSLATE
                 
-            
-            
-            
+            else if translation.x <= -60 && translation.x > -260 {
+                backgroundView.backgroundColor = UIColor.yellowColor()
+                resetAll()
+                laterIcon.alpha = 1
+                rightIconView.center = CGPoint(x: initialCenterRightIcons.x + translation.x + 60, y: initialCenterRightIcons.y)
+                endState = 1
+
                 
-            
-                
-                //MAKE YELLOW + LATER ICON TRANSLATE
-                
-                 else if translation.x <= -60 {
-                    backgroundView.backgroundColor = UIColor.yellowColor()
                 }
                 
-                //MAKE BROWN + LIST ICON TRANSLATE
-                else if translation.x <= -260 {
-                    backgroundView.backgroundColor = UIColor.brownColor()
-            }
+                //BROWN + LIST ICON TRANSLATE
                 
-            
-            
+            else if translation.x <= -260 {
+                backgroundView.backgroundColor = UIColor.brownColor()
+                resetAll()
+                listIcon.alpha = 1
+                rightIconView.center = CGPoint(x: initialCenterRightIcons.x + translation.x + 60, y: initialCenterRightIcons.y)
+                endState = 2
+            }
+
             
         }
             
         else if sender.state == UIGestureRecognizerState.Ended {
-            
+            leftIconsView.center = initialCenterLeftIcons
+            rightIconView.center = initialCenterRightIcons
             messageView.center = initialCenter
             
+            if endState == 0 {
+                UIView.animateWithDuration(0.5, animations: { () -> Void in
+                    
+                    self.feedImageView.center.y  = self.feedImageView.center.y  - 86
+                })
+            }
+            
+            
+            if endState == 1 {
+                
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.rescheduleView.alpha = 1
+                    
+                })
+                
+                
+                
+            }
+            if endState == 2 {
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.listView.alpha = 1
+                })
+                
+                
+            }
         }
         
         
     }
     
+    func resetAll () {
+        archiveIcon.alpha = 0
+        laterIcon.alpha = 0
+        deleteIcon.alpha = 0
+        listIcon.alpha = 0
+    }
+    
+    @IBAction func didTapReschedule(sender: UITapGestureRecognizer) {
+        rescheduleView.alpha = 0
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.feedImageView.center.y  = self.feedImageView.center.y  - 86
+        }
+    }
 
+    @IBAction func didTapList(sender: UITapGestureRecognizer) {
+        listView.alpha = 0
+        UIView.animateWithDuration(0.5) { () -> Void in
+            self.feedImageView.center.y  = self.feedImageView.center.y  - 86
+        }
+    }
+    
     
     }
 
